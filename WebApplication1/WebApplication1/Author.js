beee8ecd1c445e937ecbbecc84c90b535fd4c7d8
garv1911 <gauravgoyal129@gmail.com>
2016-02-18 22:42:26 +0530

    Slide 16

---
+++ WebApplication1/WebApplication1/Author.js
@@ -0,0 +1,11 @@
+﻿var AuthorApp = angular.module("AuthorApp", []);  //Creating Module
+
+AuthorApp.controller('AuthorController', function ($scope) {  //Creating Controller and Controller is a function that takes $scope as parameter
+    $scope.authors =
+        [
+            { 'name': 'Gaurav Goyal' },
+            { 'name': 'Tejas Garg' },
+            { 'name': 'Prashant Agarwal' },
+            { 'name': 'Tony' }
+        ];
+});
